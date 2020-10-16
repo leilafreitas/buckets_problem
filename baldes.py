ff6109415aa1d77c386f773af35c58e0a1eb8338
leilafreitas <leilafreitas159@gmail.com>
2020-10-16 00:19:02 -0300

    -atualizado

--- baldes.py
+++ baldes.py
@@ -2,67 +2,52 @@ import numpy as np
 import matplotlib.pyplot
 import random
 
-tm = 0.1
+import time
 
-lim = 4
-tp = 5
+inicio = time.time()
 
+
+tm = 0.1 
+lim = 4 
+tp = 100 
 
 '''ESSA LISTA CONTES OS PESOS PARA SERES DIVIDIDOS ENTRE OS BALDES, PARA ESSE CASO, SERIAM NECESSÁRIOS 3 BALDES'''
 '''São necessarios 8 baldes, para esse exemplo aqui, ou seja, é pra sobrar 4 baldes'''
 '''lista = [2, 2, 1, 4, 2, 3, 2, 2, 1, 2, 4, 4]'''
-'''lista = [2, 2, 1, 4, 2, 3, 2, 2, 1, 2, 1, 1, 4, 4, 3, 2, 1]'''
+
+'''lista = [2, 2, 1, 4, 2, 3]'''
 lista = []
-for j in range(5):
+for j in range(10):
   lista.append(random.randint(1, 4))
-'lista=[1, 3, 3, 4, 2, 1, 1, 3, 4, 1, 1, 4, 3, 2, 2, 2, 3, 4, 3, 4, 4, 4, 1, 3, 4, 3, 1, 2, 4, 1]'
 print('LISTA')
 print(lista)
 tam_lista = len(lista)
 
 
-'''def gpopulacao_i():
-    populacao = []
-    global tp
-    global tam_lista
+def modificacao_sexual(gene):
     global lista
     global lim
-    
-    cheio = np.zeros((1, tam_lista), dtype=int) 
-    for x in range(tp):
-        gene = []
-        for j in range(tam_lista):
-            x=False
-            while x == False:
-                sort = random.randint(0, tam_lista-1)
-                if (cheio[0][sort] + lista[j]) <= lim:
-                    cheio[0][sort]= cheio[0][sort] + lista[j]
-                    x = True
-            gene.append(sort)  
-        populacao.append(gene)
-    return populacao '''
+    index = random.randint(0, len(lista)-1)
+    valor = lista[index]
+    cont = 0
+    for j in lista:
+        if(j+valor) <= lim:
+            gene[index] = cont
+            break
+        cont = cont+1
+    return gene
+
 
 def gpopulacao_i():
     populacao = []
     global tp
     global tam_lista
-    global lista
-    global lim
-    b=[]
-    for p in range(tam_lista):
-        b.append(p)
     for x in range(tp):
         gene = []
-        cheio = np.zeros((1, tam_lista), dtype=int) 
         for j in range(tam_lista):
-            index = (random.randint(0, len(b) - 1))
-            value=b[index]
-            if (cheio[0][value] + lista[j]) <= lim:
-                    cheio[0][value]= cheio[0][value] + lista[j]
-                    if (cheio[0][value]==lim):
-                        b.remove(value)
-            gene.append(value)  
-        populacao.append(gene)
+            gene.append(j)
+        gene_m = modificacao_sexual(gene)
+        populacao.append(gene_m)
     return populacao 
 
 
@@ -87,8 +72,8 @@ def definir_fitness(gene):
 
 def escolha(popul):
     global tp
-    index1 = random.randint(74, (tp-1))
-    index2 = random.randint(74, (tp-1))
+    index1 = random.randint((tp*0.85-1), (tp-1))
+    index2 = random.randint((tp*0.85-1), (tp-1))
     if(definir_fitness(populacao[index1]) > definir_fitness(populacao[index2])):
         return populacao[index1]
     return populacao[index2]  
@@ -112,27 +97,29 @@ def mutate(child):
     child[x] = y
     return child
 
-print('POPULACAO')
+
 populacao = gpopulacao_i()
 populacao.sort(key=lambda s: definir_fitness(s))
-print (populacao)
-'''print (populacao)
 graph = []
-for i in range(100):
-    next_generation = []
+for i in range(100): 
+    next_generation = [] 
     for i in range(tp):
         mae1 = escolha(populacao)
         mae2 = escolha(populacao)
-        femea = sexual(mae1, mae2)
-        if(random.random() < tm):
-            femea = mutate(femea)
+        femea = sexual(mae1, mae2) 
+        if(random.random() < tm): 
+            femea = mutate(femea) 
         next_generation.append(femea)
-    populacao = next_generation
+    populacao = next_generation 
     populacao.sort(key=lambda s: definir_fitness(s))
-    graph.append(definir_fitness(populacao[99]))
+    graph.append(definir_fitness(populacao[99])) 
 matplotlib.pyplot.plot(graph)
-print(populacao)
+print(populacao) 
 print("NUMEROS DE BALDES USADOS:")
-print(tam_lista - definir_fitness(populacao[99]))
+print(tam_lista - definir_fitness(populacao[99])) 
+print("TEMPO EM SEGUNDOS:")
+fim = time.time()
+print(fim - inicio)
 matplotlib.pyplot.show()
-'''
+
+
